15f6cec4003bfd31f1eb03d8581885ecb7bd47c4
l0agha0aghayev0l <l.agha.aghayev.l@gmail.com>
2023-05-13 14:07:31 +0400

    Create Main.java

---
+++ SmackDown/src/project1/Main.java
@@ -0,0 +1,10 @@
+package project1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello world!");
+    }
+}
