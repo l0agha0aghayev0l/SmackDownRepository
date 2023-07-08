c7f3906bf3228f1bd9260ed934339e0177aedf78
l0agha0aghayev0l <l.agha.aghayev.l@gmail.com>
2023-07-08 14:00:17 +0400

    Create Main.java

---
+++ SmackDown/src/project8/Main.java
@@ -0,0 +1,55 @@
+package project8;
+
+import project8.model.Numb;
+import project8.model.Number;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Thread thread1 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i = 0; i < 100; i++) {
+//                    System.out.println(1);
+//                }
+//            }
+//        });
+//
+//        Thread thread2 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i = 0; i < 100; i++) {
+//                    System.out.println(0);
+//                }
+//            }
+//        });
+//
+//        Thread thread3 = new Thread(new Numb());
+//
+//
+//        thread1.start();
+//        thread2.start();
+//        thread3.start();
+
+//        File dir = new File("com");
+//        File file = new File("com/java/dea/code/projects/index3.txt");
+//
+//        System.out.println(file.exists());
+//        System.out.println(file.isDirectory());
+//        try {
+//            dir.mkdirs();
+//            file.createNewFile();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        File[] arr = dir.listFiles();
+//
+//        for (File f : arr) {
+//            System.out.println(f);
+//        }
+    }
+}
