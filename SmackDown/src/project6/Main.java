f4d5f1f7e8d1fbd5d5139f43b5e60d6d49c890b8
l0agha0aghayev0l <l.agha.aghayev.l@gmail.com>
2023-07-08 13:54:17 +0400

    Create Main.java

---
+++ SmackDown/src/project6/Main.java
@@ -0,0 +1,32 @@
+package project6;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+//        Date date = new Date();
+//        //        try {
+////            Thread.sleep(3000);
+////        } catch (InterruptedException e) {
+////            throw new RuntimeException(e);
+////        }
+//        Date date = new Date();
+//        System.out.println(date);
+//
+//        System.out.println(LocalDate.now());
+//
+//        System.out.println(LocalTime.now());
+//
+//        System.out.println(LocalDateTime.now());
+
+
+
+
+    }
+}
