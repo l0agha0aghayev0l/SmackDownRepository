91fbc0f8ed351b3e72c06cb0019c33d3c719d875
l0agha0aghayev0l <l.agha.aghayev.l@gmail.com>
2023-07-08 14:58:28 +0400

    Create Main.java

---
+++ SmackDown/src/project13/Practice/Exercise1/Main.java
@@ -0,0 +1,31 @@
+package project13.Practice.Exercise1;
+
+import project13.Practice.Exercise1.Models.Calc;
+import project13.Practice.Exercise1.Models.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+//        Calc calc = new Calc();
+//
+//        try {
+//            calc.calculate(12, '/', 2);
+//        } catch (ArithmeticException e){
+//            System.out.println("На ноль делить нельзя");
+//        }
+
+        Calculator calculator = new Calculator();
+
+        try {
+            calculator.calculate(12, '/', 10);
+        } catch (ArithmeticException e){
+            System.out.println("На ноль делить нельзя");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+//        try {
+//            calculator.calculate(12, '.', 45);
+//        } catch (NoSuchFieldException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
