4fb2de37ac2529bf150f9db5ae73411d8880442d
l0agha0aghayev0l <l.agha.aghayev.l@gmail.com>
2023-07-08 13:50:46 +0400

    Create Main.java

---
+++ SmackDown/src/project12/Main.java
@@ -0,0 +1,31 @@
+package project12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+       List<String> students = new ArrayList<>();
+       students.add("Alif");
+       students.add("Yusif");
+       students.add("Medina");
+       students.add("Aylin");
+       students.add("Murad");
+       students.add("Altay");
+       students.add("Rustam");
+       students.add("Omar");
+
+//        for (String s: students) {
+//            if (s.length() % 2 == 0){
+//                System.out.println(s);
+//            }
+//        }
+
+//        for (String s: students) {
+//            char[] arr = s.toCharArray();
+//            if (arr.length % 2 == 0){
+//                System.out.println(s);
+//            }
+//        }
+    }
+}
