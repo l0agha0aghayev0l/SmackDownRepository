fd5b42383c34c9dbece780b3979f4fd00605642f
l0agha0aghayev0l <l.agha.aghayev.l@gmail.com>
2023-07-08 13:50:42 +0400

    Create Subject.java

---
+++ SmackDown/src/project10/Models/Subject.java
@@ -0,0 +1,13 @@
+package project10.Models;
+
+public class Subject {
+    private int math;
+    private int history;
+
+
+
+    public Subject(int math, int history) {
+        this.math = math;
+        this.history = history;
+    }
+}
