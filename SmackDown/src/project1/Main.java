21f547da13c06208ac1ee95c51d3f7c1337abb2d
l0agha0aghayev0l <l.agha.aghayev.l@gmail.com>
2023-07-08 13:49:46 +0400

    Update Main.java

--- SmackDown/src/project1/Main.java
+++ SmackDown/src/project1/Main.java
@@ -1,10 +1,13 @@
 package project1;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello world!");
+        String s= "Hello World";
+        String s2= String.valueOf(12.5);
+        System.out.println(s2);
     }
 }
